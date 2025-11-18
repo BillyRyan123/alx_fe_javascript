67383fb5bb85abdd74cbff5f1b0fd3a7b7a797ee
billyryan123 <samfie123@outlook.com>
2025-11-18 22:06:55 +0000

    week 9 assignment

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <MainContent /> 
+      <Footer />
+      <WelcomeMessage /> 
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </>
+  )
+}
+
+export default App
